128e4817decde6b4c850f8c926d79763b92b5203
이정환 <king199777@gmail.com>
2022-02-23 13:02:17 +0900

    [Fix] : Change chromium module system

--- lib/common/image.ts
+++ lib/common/image.ts
@@ -1,11 +1,49 @@
-import { chromium } from "playwright";
+// import { Chromium } from "playwright";
+
+import Chromium from "chrome-aws-lambda";
+
+async function getBrowserInstance() {
+  const executablePath = await Chromium.executablePath;
+
+  if (!executablePath) {
+    // running locally
+    const puppeteer = require("puppeteer");
+    return puppeteer.launch({
+      args: Chromium.args,
+      headless: true,
+      defaultViewport: {
+        width: 1200,
+        height: 630,
+      },
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return Chromium.puppeteer.launch({
+    args: Chromium.args,
+    defaultViewport: {
+      width: 1200,
+      height: 630,
+    },
+    executablePath,
+    headless: Chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
+}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function generateImageFromURL(url: string) {
-  const browser = await chromium.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.setViewportSize({ width: 1200, height: 630 });
-  await page.goto(url, { waitUntil: "networkidle" });
-  const buffer = await page.screenshot({ type: "png" });
-  return buffer;
+  let browser = null;
+  browser = await getBrowserInstance();
+  let page = await browser.newPage();
+  await page.goto(url);
+  const imageBuffer = await page.screenshot();
+  return imageBuffer;
+
+  // const browser = await Chromium.launch({ headless: true });
+  // const page = await browser.newPage();
+  // await page.setViewportSize({ width: 1200, height: 630 });
+  // await page.goto(url, { waitUntil: "networkidle" });
+  // const buffer = await page.screenshot({ type: "png" });
+  // return buffer;
 }
